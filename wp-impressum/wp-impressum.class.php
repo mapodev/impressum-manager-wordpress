b847c98e26fe7938d79a55c096f28d1ff7db4e1e
Marcin Poholski <marcin.poholski@gmail.com>
2015-05-04 23:36:43 +0200

    added some new lines for the impressum. register, contact, vat etc.

--- wp-impressum/wp-impressum.class.php
+++ wp-impressum/wp-impressum.class.php
@@ -7,12 +7,19 @@ class WPImpressum
 
     private static $_de_format_address = "<h2>Angaben gemäß § 5 TMG:</h2><p>%s<br />%s<br />%s<br />%s %s</p>";
     private static $_de_format_representant = "<h2>Vertreten durch:</h2><p>[Vertreten durch: %s, %s]</p>";
-    private static $_de_format_contact = "<h2>Kontakt:</h2><table><tbody><tr><td>Telefon:</td><td>%s</td></tr><tr><td>Telefax:</td><td>%s</td></tr><tr><td>E-Mail:</td><td>%s</td></tr></tbody></table>";
-    private static $_de_format_register = "<h2>Registereintrag:</h2><p>Eintragung im %s. <br>Registergericht:%s<br>Registernummer: %s</p>";
+    private static $_de_format_contact = "<h2>Kontakt:</h2>";
+    private static $_de_format_contact_telephone = "<tr><td>Telefon:</td><td>%s</td></tr>";
+    private static $_de_format_contact_telefax = "<tr><td>Telefax:</td><td>%s</td></tr>";
+    private static $_de_format_contact_email = "<tr><td>E-Mail:</td><td>%s</td></tr>";
+    private static $_de_format_register = "<h2>Registereintrag:</h2>";
+    private static $_de_format_register_registered_in = "Eintragung im %s.";
+    private static $_de_format_register_registernr = "<br>Registernummer: %s";
+    private static $_de_format_register_chamber = "<br>Registergericht: %s";
     private static $_de_format_vat = "<h2>Umsatzsteuer-ID:</h2><p>Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:<br>%s</p>";
     private static $_de_format_office = "<h2>Aufsichtsbehörde:</h2><p>Aufsichtsbehörde</p><p>Berufsbezeichnung: gesetzl. Berufsbezeichnung:<br>Zuständige Kammer: %s<br>Verliehen durch: %s<br>Es gelten folgende berufsrechtliche Regelungen: %s<br></p>";
 
     private static $_de_source = '<p>Quelle: <em><a rel="nofollow" href="http://www.e-recht24.de/impressum-generator.html">http://www.e-recht24.de</a></em></p>';
+    private static $_de_plugin_by = '<p>Plugin von <a href="http://www.wp-impressum.com">WP-Impressum</a></p>';
 
     function __construct()
     {
@@ -37,6 +44,11 @@ class WPImpressum
     {
         $impressum = "";
 
+        $lang = strtolower(get_option("wp_impressum_language_of_impressum"));
+
+        // standard language, can be changed later on
+        if(empty($lang)) $lang = "de";
+
         $name = get_option("wp_impressum_name_company");
         $address = get_option("wp_impressum_address");
         $address_extra = get_option("wp_impressum_address_extra");
@@ -44,18 +56,28 @@ class WPImpressum
         $zip = get_option("wp_impressum_zip");
 
         if(!empty($name) && !empty($address) && !empty($address_extra) && !empty($place) && !empty($zip)) {
-            $impressum = $this->wpimpressum_return_adress("de", $name, $address, $address_extra, $place, $zip);
+            $impressum = $this->wpimpressum_return_address($lang, $name, $address, $address_extra, $place, $zip);
         }
 
+        // params for contact
         $telefon = get_option("wp_impressum_phone");
         $fax = get_option("wp_impressum_fax");
         $email = get_option("wp_impressum_email");
 
-        if(!empty($telefon)) {
-            $impressum .= $this->wpimpressum_return_contact("de", $telefon, $fax, $email);
-        }
+        // params for vat
+        $vat = get_option("wp_impressum_vat");
+
+        // params for register
+        $register = get_option("wp_impressum_register");
+        $registernr = get_option("wp_impressum_registenr");
+        $chamber = get_option("wp_impressum_chamber");
+
+        $impressum .= $this->wpimpressum_return_contact($lang, $telefon, $fax, $email);
+        $impressum .= $this->wpimpressum_return_vat($lang, $vat);
+        $impressum .= $this->wpimpressum_return_register($lang, $chamber, $registernr, $register);
 
         $impressum .= self::$_de_source;
+        $impressum .= self::$_de_plugin_by;
 
         $page = get_page_by_title("Impressum");
         $id = $page->ID;
@@ -69,7 +91,7 @@ class WPImpressum
         }
     }
 
-    private function wpimpressum_return_adress($lang, $name, $adress, $adress_extra, $place, $zip) {
+    private function wpimpressum_return_address($lang, $name, $adress, $adress_extra, $place, $zip) {
         return sprintf(self::${'_'.$lang.'_format_address'}, $name, $adress, $adress_extra, $place, $zip);
     }
 
@@ -78,6 +100,27 @@ class WPImpressum
     }
 
     private function wpimpressum_return_contact($lang, $telefon, $fax, $email) {
-        return sprintf(self::${'_'.$lang.'_format_contact'}, $telefon, $fax, $email);
+        $r = self::${'_'.$lang.'_format_contact'};
+        $r .= "<table><tbody>";
+        if(!empty($telefon)) $r .= sprintf(self::${'_'.$lang.'_format_contact_telephone'}, $telefon);
+        if(!empty($fax)) $r .= sprintf(self::${'_'.$lang.'_format_contact_telefax'}, $fax);
+        if(!empty($email)) $r .= sprintf(self::${'_'.$lang.'_format_contact_email'}, $email);
+        $r .= "</tbody></table>";
+        return $r;
+    }
+
+    private function wpimpressum_return_register($lang, $register_chamber, $registernr, $register) {
+        $r = self::${'_'.$lang.'_format_register'};
+        $r .= "<p>";
+        if(!empty($register)) $r .= sprintf(self::${'_'.$lang.'_format_register_registered_in'}, $register);
+        if(!empty($registernr)) $r .= sprintf(self::${'_'.$lang.'_format_register_registernr'}, $registernr);
+        if(!empty($register_chamber)) $r .= sprintf(self::${'_'.$lang.'_format_register_chamber'}, $register_chamber);
+        $r .= "</p>";
+        return $r;
+    }
+
+    private function wpimpressum_return_vat($lang, $vat) {
+        if(!empty($vat)) return sprintf(self::${'_'.$lang.'_format_vat'}, $vat);
+        return "";
     }
 }
