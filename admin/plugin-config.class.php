f7f16983a696e56c887da3cb64bd2dd91bd8d23f
Marcin Poholski <marcin.poholski@gmail.com>
2015-06-08 22:50:26 +0200

    using wordpress given checked function

--- admin/plugin-config.class.php
+++ admin/plugin-config.class.php
@@ -757,7 +757,7 @@ class Impressum_Manager_Config
             <table class="form-table">
                 <tr valign="top">
                     <th scope="row" colspan="2"><input type="checkbox" id="regulated_profession"
-                                                       name="impressum_manager_regulated_profession_checked" <?= $this->isChecked("impressum_manager_regulated_profession_checked") ?>><label
+                                                       name="impressum_manager_regulated_profession_checked" <?= checked("on", get_option("impressum_manager_regulated_profession_checked"), false) ?>><label
                             for="regulated_profession"><b><?= __("Reglementierter Beruf", $this->slug) ?></b></label>
                     </th>
                 </tr>
@@ -794,7 +794,7 @@ class Impressum_Manager_Config
                 </tr>
                 <tr valign="top">
                     <th scope="row" colspan="2"><input type="checkbox" id="allowness"
-                                                       name="impressum_manager_allowness" <?= $this->isChecked("impressum_manager_allowness") ?>><label
+                                                       name="impressum_manager_allowness" <?= checked("on", get_option("impressum_manager_allowness"), false) ?>><label
                             for="allowness"><b><?= __("Behördliche Zuslassung", $this->slug) ?></b></label></th>
                 </tr>
                 <tr valign="top" id="allowness_textarea">
@@ -819,7 +819,7 @@ class Impressum_Manager_Config
 
                 <tr valign="top">
                     <th scope="row" colspan="2"><input type="checkbox" id="press_content"
-                                                       name="impressum_manager_press_content" <?= $this->isChecked("impressum_manager_press_content") ?>
+                                                       name="impressum_manager_press_content" <?= checked("on", get_option("impressum_manager_press_content"), false) ?>
                         <label
                             for="press_content"><b><?= __("journalistisch-redaktionelle Inhalte", $this->slug) ?></b></label>
                     </th>
@@ -883,7 +883,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_disclaimer">
                             <input id="impressum_manager_disclaimer" type="checkbox"
-                                   name="impressum_manager_disclaimer" <?= $this->isChecked("impressum_manager_disclaimer") ?>>
+                                   name="impressum_manager_disclaimer" <?= checked("on", get_option("impressum_manager_disclaimer"), false) ?>>
                             <?= __("Füge einen Disclaimer in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -895,7 +895,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_general_privacy_policy">
                             <input id="impressum_manager_general_privacy_policy" type="checkbox"
-                                   name="impressum_manager_general_privacy_policy" <?= $this->isChecked("impressum_manager_general_privacy_policy") ?>>
+                                   name="impressum_manager_general_privacy_policy" <?= checked("on", get_option("impressum_manager_general_privacy_policy"), false) ?>>
                             <?= __("Füge eine allgemeine Datenschutzerklärung in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -907,7 +907,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_policy_facebook">
                             <input id="impressum_manager_policy_facebook" type="checkbox"
-                                   name="impressum_manager_policy_facebook" <?= $this->isChecked("impressum_manager_policy_facebook") ?>>
+                                   name="impressum_manager_policy_facebook" <?= checked("on",get_option("impressum_manager_policy_facebook"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von Facebook Elementen in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -919,19 +919,19 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_policy_google_analytics">
                             <input id="impressum_manager_policy_google_analytics" type="checkbox"
-                                   name="impressum_manager_policy_google_analytics" <?= $this->isChecked("impressum_manager_policy_google_analytics") ?>>
+                                   name="impressum_manager_policy_google_analytics" <?= checked("on",get_option("impressum_manager_policy_google_analytics"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von <b>Google Analytics</b> in dein Impressum ein.", $this->slug) ?>
                         </label>
                         <br><br>
                         <label for="impressum_manager_policy_google_adsense">
                             <input id="impressum_manager_policy_google_adsense" type="checkbox"
-                                   name="impressum_manager_policy_google_adsense" <?= $this->isChecked("impressum_manager_policy_google_adsense") ?>>
+                                   name="impressum_manager_policy_google_adsense" <?= checked("on", get_option("impressum_manager_policy_google_adsense"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von <b>Google Adsense</b> in dein Impressum ein.", $this->slug) ?>
                         </label>
                         <br><br>
                         <label for="impressum_manager_policy_google_plus">
                             <input id="impressum_manager_policy_google_plus" type="checkbox"
-                                   name="impressum_manager_policy_google_plus" <?= $this->isChecked("impressum_manager_policy_google_plus") ?>>
+                                   name="impressum_manager_policy_google_plus" <?= checked("on",get_option("impressum_manager_policy_google_plus"), false)?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von <b>Google +1</b> in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -943,7 +943,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_policy_twitter">
                             <input id="impressum_manager_policy_twitter" type="checkbox"
-                                   name="impressum_manager_policy_twitter" <?= $this->isChecked("impressum_manager_policy_twitter") ?>>
+                                   name="impressum_manager_policy_twitter" <?= checked("on", get_option("impressum_manager_policy_twitter"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von Twitter Elementen in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -1199,7 +1199,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_noindex">
                             <input id="impressum_manager_noindex" type="checkbox"
-                                   name="impressum_manager_noindex" <?= $this->isChecked("impressum_manager_noindex") ?>>
+                                   name="impressum_manager_noindex" <?= checked("on", get_option("impressum_manager_noindex"), false) ?>>
                             <?= __("Lass die Impressum Seite nicht von Suchmaschinen indexieren.", $this->slug) ?>
                         </label>
                     </td>
@@ -1209,7 +1209,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_show_email_as_image">
                             <input id="impressum_manager_show_email_as_image" type="checkbox"
-                                   name="impressum_manager_show_email_as_image" <?= $this->isChecked("impressum_manager_show_email_as_image") ?>>
+                                   name="impressum_manager_show_email_as_image" <?= checked("on", get_option("impressum_manager_show_email_as_image"), false) ?>>
                             <?= __("Show E-Mail as Image to prevent Spam.", $this->slug) ?>
                         </label>
                     </td>
@@ -1224,7 +1224,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_disclaimer">
                             <input id="impressum_manager_disclaimer" type="checkbox"
-                                   name="impressum_manager_disclaimer" <?= $this->isChecked("impressum_manager_disclaimer") ?>>
+                                   name="impressum_manager_disclaimer" <?= checked("on", get_option("impressum_manager_disclaimer"), false) ?>>
                             <?= __("Füge einen Disclaimer in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -1236,7 +1236,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_general_privacy_policy">
                             <input id="impressum_manager_general_privacy_policy" type="checkbox"
-                                   name="impressum_manager_general_privacy_policy" <?= $this->isChecked("impressum_manager_general_privacy_policy") ?>>
+                                   name="impressum_manager_general_privacy_policy" <?= checked("on", get_option("impressum_manager_general_privacy_policy"), false) ?>>
                             <?= __("Füge eine allgemeine Datenschutzerklärung in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -1248,7 +1248,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_policy_facebook">
                             <input id="impressum_manager_policy_facebook" type="checkbox"
-                                   name="impressum_manager_policy_facebook" <?= $this->isChecked("impressum_manager_policy_facebook") ?>>
+                                   name="impressum_manager_policy_facebook" <?= checked("on", get_option("impressum_manager_policy_facebook"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von Facebook Elementen in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -1260,19 +1260,19 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_policy_google_analytics">
                             <input id="impressum_manager_policy_google_analytics" type="checkbox"
-                                   name="impressum_manager_policy_google_analytics" <?= $this->isChecked("impressum_manager_policy_google_analytics") ?>>
+                                   name="impressum_manager_policy_google_analytics" <?= checked("on", get_option("impressum_manager_policy_google_analytics"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von <b>Google Analytics</b> in dein Impressum ein.", $this->slug) ?>
                         </label>
                         <br><br>
                         <label for="impressum_manager_policy_google_adsense">
                             <input id="impressum_manager_policy_google_adsense" type="checkbox"
-                                   name="impressum_manager_policy_google_adsense" <?= $this->isChecked("impressum_manager_policy_google_adsense") ?>>
+                                   name="impressum_manager_policy_google_adsense" <?= checked("on", get_option("impressum_manager_policy_google_adsense"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von <b>Google Adsense</b> in dein Impressum ein.", $this->slug) ?>
                         </label>
                         <br><br>
                         <label for="impressum_manager_policy_google_plus">
                             <input id="impressum_manager_policy_google_plus" type="checkbox"
-                                   name="impressum_manager_policy_google_plus" <?= $this->isChecked("impressum_manager_policy_google_plus") ?>>
+                                   name="impressum_manager_policy_google_plus" <?= checked("on", get_option("impressum_manager_policy_google_plus"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von <b>Google +1</b> in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -1284,7 +1284,7 @@ class Impressum_Manager_Config
                     <td>
                         <label for="impressum_manager_policy_twitter">
                             <input id="impressum_manager_policy_twitter" type="checkbox"
-                                   name="impressum_manager_policy_twitter" <?= $this->isChecked("impressum_manager_policy_twitter") ?>>
+                                   name="impressum_manager_policy_twitter" <?= checked("on", get_option("impressum_manager_policy_twitter"), false) ?>>
                             <?= __("Füge eine Datenschutzerklärung für die Nutzung von Twitter Elementen in dein Impressum ein.", $this->slug) ?>
                         </label>
                     </td>
@@ -1303,12 +1303,5 @@ class Impressum_Manager_Config
             <?php submit_button(__("Impressum aktualisieren")); ?>
         </form>
     <?php
-    }
-
-    private function isChecked($key)
-    {
-        if (strlen(get_option($key)) > 0) {
-            echo "checked=checked";
-        }
     }
 }
