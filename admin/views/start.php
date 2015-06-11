3aa32b73a2e5e776e967f456731d98106006adc5
cj <cj4hunt@gmail.com>
2015-06-11 14:08:51 +0200

    logo

--- admin/views/start.php
+++ admin/views/start.php
@@ -6,9 +6,12 @@ http://www.davidjrush.com/blog/2011/12/simple-jquery-tooltip/
 Zu den Einstellungen <großer zwischenraum> schritt zurück <zwischenraum> nächster schritt
 
 */
+
 ?>
 
+<h2 class="logo">'.esc_html__('Impressum Manager', SLUG).'</h2>
 <div class="impressum-manager-start-wrap">
+
 	<p><?php esc_html_e( 'Willkommen bei Impressum-Manager. Dieses plugin hilft dir deine Webseite(n) rechtsicher zu machen ...', SLUG ); ?></p>
 
 	<p><?php esc_html_e( 'HÄCKCHENTEXT', SLUG ); ?></p>
@@ -16,9 +19,10 @@ Zu den Einstellungen <großer zwischenraum> schritt zurück <zwischenraum> näch
 	<div class="box primary">
 		<div>
 			<strong><?php esc_html_e( 'In 4 Schritten zur rechtssicheren Webseite', SLUG ); ?></strong><br>
+
 			<p><?php esc_html_e( 'Mit dem Impressum-Manager kannst du deine Webseite schnell rechtssicher machen, indem du ...', SLUG ); ?></p>
 		</div>
-		<form action="<?php Impressum_Manager_Admin::get_page_url()?>" class="right">
+		<form action="<?php Impressum_Manager_Admin::get_page_url() ?>" class="right">
 			<input type="hidden" name="page" value="<?= SLUG ?>">
 			<input type="hidden" name="view" value="tutorial"/>
 			<input type="hidden" name="step" value="1"/>
@@ -30,9 +34,10 @@ Zu den Einstellungen <großer zwischenraum> schritt zurück <zwischenraum> näch
 	<div class="box secondary">
 		<div>
 			<strong><?php esc_html_e( 'Später Konfigurieren', SLUG ); ?></strong><br>
+
 			<p><?php esc_html_e( 'Das Impressum lässt sich auch jederzeit später konfigureren. Wenn du erst die Einstellungen sehen möchstest, klicke hier auf den Button.', SLUG ); ?></p>
 		</div>
-		<form action="<?php Impressum_Manager_Admin::get_page_url()?>" class="right">
+		<form action="<?php Impressum_Manager_Admin::get_page_url() ?>" class="right">
 			<input type="hidden" name="page" value="<?= SLUG ?>">
 			<input type="hidden" name="view" value="config">
 			<input type="hidden" name="skip_start" value="true">
--- admin/views/tutorial/page1.php
+++ admin/views/tutorial/page1.php
@@ -12,6 +12,20 @@
 		});
 	}(jQuery));
 </script>
+
+<script>
+	(function($) {
+		$(document).ready(function () {
+			$("span.question").hover(function () {
+				$(this).append('<div class="tooltip"><p>This is a tooltip. It is typically used to explain something to a user without taking up space on the page.</p></div>');
+			}, function () {
+				$("div.tooltip").remove();
+			});
+		});
+	}(jQuery));
+</script>
+
+<p>This sentence needs more of an explanation for the user. <span class="question">?</span></p>
 <br>
 
 <form action="<?= Impressum_Manager_Admin::get_page_url() ?>&view=tutorial&skip_start_temp=true&step=2" method="post">
