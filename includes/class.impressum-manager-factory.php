d8836f6b281a335e38fee061a60a8ed0ed49a740
Marcin Poholski <marcin.poholski@gmail.com>
2015-06-17 19:56:30 +0200

    formating code. willing to text added with logic to confirm text to use plugin

--- includes/class.impressum-manager-factory.php
+++ includes/class.impressum-manager-factory.php
@@ -1,48 +1,54 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: C4
  * Date: 17.06.2015
  * Time: 12:26
  */
+class Impressum_Manager_Factory
+{
 
-class Impressum_Manager_Factory {
+    public static function create_impressum()
+    {
 
-	public static function create_impressum(){
+        $impressum_configured = true;
+        $impressum_imported = true;
 
-		$impressum_configured = true;
-		$impressum_imported = true;
+        if (true == $impressum_configured) {
+            $impressum = new Impressum_Manager_Impressum();
+        } elseif (true == $impressum_imported) {
 
-		if($impressum_configured==true) {
-			$impressum = new Impressum_Manager_Impressum();
-		}elseif($impressum_imported==true){
+        } else {
 
-		}else{
+        }
+        return $impressum->get_impressum();
 
-		}
-		return $impressum->get_impressum();
+    }
 
-	}
+    public static function create_privacy_policy()
+    {
+        $im = new Impressum_Manager_Impressum();
+        return $im->get_privacy_policy();
+    }
 
-	public static function create_privacy_policy(){
-		$im = new Impressum_Manager_Impressum();
-		return $im->get_privacy_policy();
-	}
+    public static function create_disclaimer()
+    {
+        $im = new Impressum_Manager_Impressum();
+        return $im->get_disclaimer();
+    }
 
-	public static function create_disclaimer(){
-		$im = new Impressum_Manager_Impressum();
-		return $im->get_disclaimer();
-	}
+    public static function create_contact()
+    {
+        $im = new Impressum_Manager_Impressum();
+        return $im->get_contact();
+    }
 
-	public static function create_contact(){
-		$im = new Impressum_Manager_Impressum();
-		return $im->get_contact();
-	}
-
-	public static function create_image_sources(){
-		$im = new Impressum_Manager_Impressum();
-		return $im->get_image_sources();
-	}
+    public static function create_image_sources()
+    {
+        $im = new Impressum_Manager_Impressum();
+        return $im->get_image_sources();
+    }
 
 
 }
