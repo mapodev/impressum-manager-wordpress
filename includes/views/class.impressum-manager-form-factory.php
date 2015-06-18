e8e3755bb9e541982a2e753872ec5d3fbd171d3f
Marcin Poholski <marcin.poholski@gmail.com>
2015-06-18 20:53:54 +0200

    removed fieldset

--- includes/views/class.impressum-manager-form-factory.php
+++ includes/views/class.impressum-manager-form-factory.php
@@ -153,23 +153,23 @@ class Impressum_Manager_Form_Factory
             <th scope="row"><b><?= __("Art der Person", SLUG) . " " ?>
             </th>
             <td>
-                    <label>
-                        <input type="radio" id="person_1" name="impressum_manager_person"
-                               value="1" <?php
-                        if (get_option('impressum_manager_person') == 1) {
-                            echo "checked=checked";
-                        }?>
-                        <span>Privatperson</span>
-                    </label>
-                    <br>
-                    <label>
-                        <input type="radio" id="person_2" name="impressum_manager_person"
-                               value="2" <?php
-                        if (get_option('impressum_manager_person') == 2) {
-                            echo "checked=checked";
-                        }?>>
-                        <span><?= __("Juristische Person (z.B. Firma, Verein, Organisation, Einrichtung)") ?></span>
-                    </label>
+                <label>
+                    <input type="radio" id="person_1" name="impressum_manager_person"
+                           value="1" <?php
+                    if (get_option('impressum_manager_person') == 1) {
+                        echo "checked=checked";
+                    }?>
+                    <span>Privatperson</span>
+                </label>
+                <br>
+                <label>
+                    <input type="radio" id="person_2" name="impressum_manager_person"
+                           value="2" <?php
+                    if (get_option('impressum_manager_person') == 2) {
+                        echo "checked=checked";
+                    }?>>
+                    <span><?= __("Juristische Person (z.B. Firma, Verein, Organisation, Einrichtung)") ?></span>
+                </label>
 
             </td>
         </tr>
@@ -223,51 +223,51 @@ class Impressum_Manager_Form_Factory
             <th scope="row"><b><?= __("Angaben zur Organisation", SLUG) ?></b></th>
             <td>
 
-                    <input type="text" name="impressum_manager_name_company"
-                           title="Company Name"
-                           value="<?= get_option("impressum_manager_name_company") ?>">
-                    <br>
-                    <small id="full_name"><?= __("Vollständiger Name", SLUG) ?></small>
-                    <br>
-                    <input type="text" name="impressum_manager_address" title="Address"
-                           value="<?= get_option("impressum_manager_address") ?>">
-                    <br>
-                    <small><?= __("Straße & Hausnummer", SLUG) ?></small>
-                    <br>
-                    <input type="text" name="impressum_manager_address_extra" title="Address Extra"
-                           value="<?= get_option("impressum_manager_address_extra") ?>"><br>
-                    <small><?= __("Adresszusatz", SLUG) ?></small>
-                    <br>
-                    <input type="text" name="impressum_manager_place"
-                           title="Place"
-                           value="<?= get_option("impressum_manager_place") ?>"><br>
-                    <small><?= __("Ort") ?></small>
-                    <br>
-                    <input type="text" name="impressum_manager_zip"
-                           title="ZIP Code"
-                           value="<?= get_option("impressum_manager_zip") ?>"><br>
-                    <small><?= __("PLZ") ?></small>
-                    <br>
-                    <select name="impressum_manager_country">
-                        <option value="no_land_choosen"><?= __("Wähle dein Land ...") ?></option>
-                        <?php
+                <input type="text" name="impressum_manager_name_company"
+                       title="Company Name"
+                       value="<?= get_option("impressum_manager_name_company") ?>">
+                <br>
+                <small id="full_name"><?= __("Vollständiger Name", SLUG) ?></small>
+                <br>
+                <input type="text" name="impressum_manager_address" title="Address"
+                       value="<?= get_option("impressum_manager_address") ?>">
+                <br>
+                <small><?= __("Straße & Hausnummer", SLUG) ?></small>
+                <br>
+                <input type="text" name="impressum_manager_address_extra" title="Address Extra"
+                       value="<?= get_option("impressum_manager_address_extra") ?>"><br>
+                <small><?= __("Adresszusatz", SLUG) ?></small>
+                <br>
+                <input type="text" name="impressum_manager_place"
+                       title="Place"
+                       value="<?= get_option("impressum_manager_place") ?>"><br>
+                <small><?= __("Ort") ?></small>
+                <br>
+                <input type="text" name="impressum_manager_zip"
+                       title="ZIP Code"
+                       value="<?= get_option("impressum_manager_zip") ?>"><br>
+                <small><?= __("PLZ") ?></small>
+                <br>
+                <select name="impressum_manager_country">
+                    <option value="no_land_choosen"><?= __("Wähle dein Land ...") ?></option>
+                    <?php
 
-                        foreach (Impressum_Manager_Admin::$_countries as $country_code => $country_name) {
-                            if (get_option("impressum_manager_country") == $country_code) {
-                                $s = "selected=selected";
-                            } else {
-                                $s = "";
-                            }
-
-                            ?>
-                            <option
-                                value="<?= $country_code ?>" <?= $s ?>><?= __($country_name, SLUG) ?></option>
-                        <?php
+                    foreach (Impressum_Manager_Admin::$_countries as $country_code => $country_name) {
+                        if (get_option("impressum_manager_country") == $country_code) {
+                            $s = "selected=selected";
+                        } else {
+                            $s = "";
                         }
 
                         ?>
-                    </select><br>
-                    <small>Land</small>
+                        <option
+                            value="<?= $country_code ?>" <?= $s ?>><?= __($country_name, SLUG) ?></option>
+                    <?php
+                    }
+
+                    ?>
+                </select><br>
+                <small>Land</small>
             </td>
         </tr>
 
@@ -305,19 +305,17 @@ class Impressum_Manager_Form_Factory
                     for="professional_liability_insurance"><b><?= __("Berufshaftpflichtversicherung", SLUG) ?></b></label>
             </th>
             <td class="hide_professional_liability_insurance">
-                <fieldset>
-                    <textarea name="impressum_manager_name_and_adress"
-                        ><?= get_option("impressum_manager_name_and_adress") ?></textarea>
-                    <br>
-                    <small><?= __("Name und Anschrift", SLUG) ?>
-                    </small>
-                    <br>
-                    <input type="text" name="impressum_manager_space_of_appliance" title="State"
-                           value="<?= get_option("impressum_manager_space_of_appliance") ?>">
-                    <br>
-                    <small><?= __("Geltungsraum", SLUG) ?></small>
-                    <br>
-                </fieldset>
+                <textarea name="impressum_manager_name_and_adress"
+                    ><?= get_option("impressum_manager_name_and_adress") ?></textarea>
+                <br>
+                <small><?= __("Name und Anschrift", SLUG) ?>
+                </small>
+                <br>
+                <input type="text" name="impressum_manager_space_of_appliance" title="State"
+                       value="<?= get_option("impressum_manager_space_of_appliance") ?>">
+                <br>
+                <small><?= __("Geltungsraum", SLUG) ?></small>
+                <br>
             </td>
         </tr> <?php
     }
