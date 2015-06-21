d7ab3197a338d16709e9cd53a5d7fb578734d906
Marcin Poholski <marcin.poholski@gmail.com>
2015-06-21 20:08:50 +0200

    working uninstall, tested on website

--- uninstall.php
+++ uninstall.php
@@ -1,30 +1,5 @@
 <?php
 
-/**
- * Fired when the plugin is uninstalled.
- *
- * When populating this file, consider the following flow
- * of control:
- *
- * - This method should be static
- * - Check if the $_REQUEST content actually is the plugin name
- * - Run an admin referrer check to make sure it goes through authentication
- * - Verify the output of $_GET makes sense
- * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
- *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
- *
- * @link       http://www.mapo-dev.com
- * @since      0.1.0
- *
- * @package    impressum_manager_Plugin
- */
-
 // If uninstall not called from WordPress, then exit.
 //if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 //    exit;
@@ -128,7 +103,7 @@ unregister_setting("impressum-manager-settings-group", "impressum_manager_space_
 global $wpdb;
 // delete table
 $table_name = $wpdb->prefix . "impressum_manager_content";
-$sql = "DROP TABLE IF_EXISTS $table_name;";
+$sql = "DROP TABLE IF EXISTS {$table_name};";
 $e = $wpdb->query($sql);
 
 
